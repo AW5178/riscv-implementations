5dc1655ba7adda47c3c79b2c074fc0221f97f25b
Andrew Wolters <andrewwolters5178@gmail.com>
2024-03-06 22:52:08 -0500

    FILES MODIFIED: ajw_addsub_unit.sv tb_ajw_addsub_unit.sv
    DESC:
    Redid and verified functionality of CLA. TODO: Further Document TB and Document Sim results.

--- in-order-pipeline-ideal-memory/rtl/ajw_addsub_unit.sv
+++ in-order-pipeline-ideal-memory/rtl/ajw_addsub_unit.sv
@@ -5,7 +5,7 @@
 // File Purpose: Functional Description of 32-bit Carry-LookAhead Adder
 //////////////////////////////////////////////////////////////
 
-module ajw_32bit_addsub_unit #() (
+module ajw_addsub_unit (
     //-----------------INPUTS-------------------//
     input  logic [31:0]           opX_i, // Operand X of operation
     input  logic [31:0]           opY_i, // Operand Y of operation
@@ -14,9 +14,6 @@ module ajw_32bit_addsub_unit #() (
     output logic [31:0]           sum_o, // output sum
     output logic                  cout_o // output carry out for SR
 );
-    // recall: g[i] = x[i]&y[i] and p[i] = x[i]|y[i]
-    // c[i+1]=g[i] | p[i] & c[i]
-
     //////////////////////////////////////////////////////////////
     //                        VARIABLES
     //////////////////////////////////////////////////////////////
@@ -25,70 +22,238 @@ module ajw_32bit_addsub_unit #() (
     logic [31:0] prop_s0; // 1st layer of computed propagate functions
     logic [3:0]  gen_s1; // 2nd layer of computed generate functions
     logic [3:0]  prop_s1; // 2nd layer of computed propagate functions
-    logic [ 2:0] carry_int; // Vector of intermediate carry bits used to compute sums
+    logic [32:0] carry_int; // Vector of intermediate carry bits used to compute sums
 
     //////////////////////////////////////////////////////////////
     //                        PROCEDURES
     //////////////////////////////////////////////////////////////
     always_comb begin : c_addsub
+	     carry_int[0] = cin_i;
         // Cin = 0 -> Standard ADD
         // Cin = 1 -> Invert: 2'sC Subtraction
-        opY_int = opY ^ {32{cin_i}};
+        opY_int = opY_i ^ {32{carry_int[0]}};
         
         for (int i=0; i<32; i=i+1) begin : f_generate_calcs
-            gen_s0[i]  = opX[i] & opY_int[i]; // Generates: x & y
-            prop_s0[i] = opX[i] | opY_int[i]; // Propagates: x | y
+            gen_s0[i]  = opX_i[i] & opY_int[i]; // Generates: x & y
+            prop_s0[i] = opX_i[i] | opY_int[i]; // Propagates: x | y
         end
-
-        // block level generate and propagate functions: there are 4 each
-        for (int i=0; i<4; i=i+1) begin
-            gen_s1[i] = (gen_s0[(8*i)+7]) |
-                (gen_s0[(8*i)+6] & prop_s0[(8*i)+7]) |
-                (gen_s0[(8*i)+5] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7]) |
-                (gen_s0[(8*i)+4] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7]) |
-                (gen_s0[(8*i)+3] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7]) |
-                (gen_s0[(8*i)+2] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7]) |
-                (gen_s0[(8*i)+1] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7]) |
-                (gen_s0[(8*i)  ] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7] & prop_s0[(8*i)+7]);
-            
-            prop_s1[i] = ( prop_s0[(8*i)+7] &
-                           prop_s0[(8*i)+6] &
-                           prop_s0[(8*i)+5] &
-                           prop_s0[(8*i)+4] &
-                           prop_s0[(8*i)+3] &
-                           prop_s0[(8*i)+2] &
-                           prop_s0[(8*i)+1] &
-                           prop_s0[(8*i)  ]
-                         );
-        end
-
-        // c[8]
-        carry_int[0] = (gen_s1[0]) |
-                       (prop_s1[0] & cin_i);
+		
+        // BELOW: CARRY CALCULATIONS
+        // CARRYS ARE CALCULATED 8 AT A TIME BEFORE PROPAGATING TOA NOTHER CLA.
+        carry_int[1] = (gen_s0[0]) | 
+                       (prop_s0[0] & carry_int[0]);
         
-        // c[16]
-        carry_int[1] = (gen_s1[1])              |
-                       (prop_s1[1] & gen_s1[0]) |
-                       (prop_s1[1] & prop_s1[0] & cin_i);
+        carry_int[2] = (gen_s0[1]) | 
+                       (prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[1] & prop_s0[0] & carry_int[0]);
         
-        // c[24]
-        carry_int[2] = (gen_s1[2])                           |
-                       (prop_s1[2] & gen_s1[1])              | 
-                       (prop_s1[2] & prop_s1[1] & gen_s1[0]) | 
-                       (prop_s1[2] & prop_s1[1] & prop_s1[0] & cin_i);
-
-        // c[32] == cout_o
-        cout_o       = (gen_s1[3])                                        | 
-                       (prop_s1[3] & gen_s1[2])                           | 
-                       (prop_s1[3] & prop_s1[2] & gen_s1[1])              |
-                       (prop_s1[3] & prop_s1[2] & prop_s1[1] & gen_s1[0]) |
-                       (prop_s1[3] & prop_s1[2] & prop_s1[1] & prop_s1[0] & cin_i);
+        carry_int[3] = (gen_s0[2]) | 
+                       (prop_s0[2] & gen_s0[1]) |
+                       (prop_s0[2] & prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[2] & prop_s0[1] & prop_s0[0] & carry_int[0]);
+        
+        carry_int[4] = (gen_s0[3]) | 
+                       (prop_s0[3] & gen_s0[2]) |
+                       (prop_s0[3] & prop_s0[2] & gen_s0[1]) |
+                       (prop_s0[3] & prop_s0[2] & prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[3] & prop_s0[2] & prop_s0[1] & prop_s0[0] & carry_int[0]);
+       
+        carry_int[5] = (gen_s0[4]) |
+                       (prop_s0[4] & gen_s0[3]) | 
+                       (prop_s0[4] & prop_s0[3] & gen_s0[2]) |
+                       (prop_s0[4] & prop_s0[3] & prop_s0[2] & gen_s0[1]) |
+                       (prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & prop_s0[0] & carry_int[0]);
+    
+        carry_int[6] = (gen_s0[5]) |
+                       (prop_s0[5] & gen_s0[4]) |
+                       (prop_s0[5] & prop_s0[4] & gen_s0[3]) |
+                       (prop_s0[5] & prop_s0[4] & prop_s0[3] & gen_s0[2]) |
+                       (prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & gen_s0[1]) |
+                       (prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & prop_s0[0] & carry_int[0]);
+        
+        carry_int[7] = (gen_s0[6]) | 
+                       (prop_s0[6] & gen_s0[5]) |
+                       (prop_s0[6] & prop_s0[5] & gen_s0[4]) |
+                       (prop_s0[6] & prop_s0[5] & prop_s0[4] & gen_s0[3]) |
+                       (prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & gen_s0[2]) |
+                       (prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & gen_s0[1]) |
+                       (prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & prop_s0[0] & carry_int[0]);
+        
+        carry_int[8] = (gen_s0[7]) | 
+                       (prop_s0[7] & gen_s0[6]) |
+                       (prop_s0[7] & prop_s0[6] & gen_s0[5]) |
+                       (prop_s0[7] & prop_s0[6] & prop_s0[5] & gen_s0[4]) |
+                       (prop_s0[7] & prop_s0[6] & prop_s0[5] & prop_s0[4] & gen_s0[3]) |
+                       (prop_s0[7] & prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & gen_s0[2]) |
+                       (prop_s0[7] & prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & gen_s0[1]) |
+                       (prop_s0[7] & prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & gen_s0[0]) |
+                       (prop_s0[7] & prop_s0[6] & prop_s0[5] & prop_s0[4] & prop_s0[3] & prop_s0[2] & prop_s0[1] & prop_s0[0] & carry_int[0]);
+        
+        carry_int[9] = (gen_s0[8]) | 
+                       (prop_s0[8] & carry_int[8]);
+        
+        carry_int[10] = (gen_s0[ 9]) | 
+                        (prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[ 9] & prop_s0[8] & carry_int[8]);
+        
+        carry_int[11] = (gen_s0[10]) | 
+                        (prop_s0[10] & gen_s0[ 9]) |
+                        (prop_s0[10] & prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[10] & prop_s0[ 9] & prop_s0[8] & carry_int[8]);
+        
+        carry_int[12] = (gen_s0[11]) | 
+                        (prop_s0[11] & gen_s0[10]) |
+                        (prop_s0[11] & prop_s0[10] & gen_s0[ 9]) |
+                        (prop_s0[11] & prop_s0[10] & prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[11] & prop_s0[10] & prop_s0[ 9] & prop_s0[8] & carry_int[8]);
+        
+        carry_int[13] = (gen_s0[12]) |
+                        (prop_s0[12] & gen_s0[11]) | 
+                        (prop_s0[12] & prop_s0[11] & gen_s0[10]) |
+                        (prop_s0[12] & prop_s0[11] & prop_s0[10] & gen_s0[ 9]) |
+                        (prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & prop_s0[8] & carry_int[8]);
+    
+        carry_int[14] = (gen_s0[13]) |
+                        (prop_s0[13] & gen_s0[12]) |
+                        (prop_s0[13] & prop_s0[12] & gen_s0[11]) |
+                        (prop_s0[13] & prop_s0[12] & prop_s0[11] & gen_s0[10]) |
+                        (prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & gen_s0[ 9]) |
+                        (prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & prop_s0[8] & carry_int[8]);
+        
+        carry_int[15] = (gen_s0[14]) | 
+                        (prop_s0[14] & gen_s0[13]) |
+                        (prop_s0[14] & prop_s0[13] & gen_s0[12]) |
+                        (prop_s0[14] & prop_s0[13] & prop_s0[12] & gen_s0[11]) |
+                        (prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & gen_s0[10]) |
+                        (prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & gen_s0[ 9]) |
+                        (prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & prop_s0[8] & carry_int[8]);
+        
+        carry_int[16] = (gen_s0[15]) | 
+                        (prop_s0[15] & gen_s0[14]) |
+                        (prop_s0[15] & prop_s0[14] & gen_s0[13]) |
+                        (prop_s0[15] & prop_s0[14] & prop_s0[13] & gen_s0[12]) |
+                        (prop_s0[15] & prop_s0[14] & prop_s0[13] & prop_s0[12] & gen_s0[11]) |
+                        (prop_s0[15] & prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & gen_s0[10]) |
+                        (prop_s0[15] & prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & gen_s0[ 9]) |
+                        (prop_s0[15] & prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[ 9] & gen_s0[8]) |
+                        (prop_s0[15] & prop_s0[14] & prop_s0[13] & prop_s0[12] & prop_s0[11] & prop_s0[10] & prop_s0[9 ] & prop_s0[8] & carry_int[8]);
+        
+        carry_int[17] = (gen_s0[16]) | 
+                        (prop_s0[16] & carry_int[16]);
+        
+        carry_int[18] = (gen_s0[17]) | 
+                        (prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[17] & prop_s0[16] & carry_int[16]);
+        
+        carry_int[19] = (gen_s0[18]) | 
+                        (prop_s0[18] & gen_s0[17]) |
+                        (prop_s0[18] & prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[18] & prop_s0[17] & prop_s0[16] & carry_int[16]);
+        
+        carry_int[20] = (gen_s0[19]) | 
+                        (prop_s0[19] & gen_s0[18]) |
+                        (prop_s0[19] & prop_s0[18] & gen_s0[17]) |
+                        (prop_s0[19] & prop_s0[18] & prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[19] & prop_s0[18] & prop_s0[17] & prop_s0[16] & carry_int[16]);
+        
+        carry_int[21] = (gen_s0[20]) |
+                        (prop_s0[20] & gen_s0[19]) | 
+                        (prop_s0[20] & prop_s0[19] & gen_s0[18]) |
+                        (prop_s0[20] & prop_s0[19] & prop_s0[18] & gen_s0[17]) |
+                        (prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & prop_s0[16] & carry_int[16]);
+    
+        carry_int[22] = (gen_s0[21]) |
+                        (prop_s0[21] & gen_s0[20]) |
+                        (prop_s0[21] & prop_s0[20] & gen_s0[19]) |
+                        (prop_s0[21] & prop_s0[20] & prop_s0[19] & gen_s0[18]) |
+                        (prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & gen_s0[17]) |
+                        (prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & prop_s0[16] & carry_int[16]);
+        
+        carry_int[23] = (gen_s0[22]) | 
+                        (prop_s0[22] & gen_s0[21]) |
+                        (prop_s0[22] & prop_s0[21] & gen_s0[20]) |
+                        (prop_s0[22] & prop_s0[21] & prop_s0[20] & gen_s0[19]) |
+                        (prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & gen_s0[18]) |
+                        (prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & gen_s0[17]) |
+                        (prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & prop_s0[16] & carry_int[16]);
+        
+        carry_int[24] = (gen_s0[23]) | 
+                        (prop_s0[23] & gen_s0[22]) |
+                        (prop_s0[23] & prop_s0[22] & gen_s0[21]) |
+                        (prop_s0[23] & prop_s0[22] & prop_s0[21] & gen_s0[20]) |
+                        (prop_s0[23] & prop_s0[22] & prop_s0[21] & prop_s0[20] & gen_s0[19]) |
+                        (prop_s0[23] & prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & gen_s0[18]) |
+                        (prop_s0[23] & prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & gen_s0[17]) |
+                        (prop_s0[23] & prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & gen_s0[16]) |
+                        (prop_s0[23] & prop_s0[22] & prop_s0[21] & prop_s0[20] & prop_s0[19] & prop_s0[18] & prop_s0[17] & prop_s0[16] & carry_int[16]);
+    
+        carry_int[25] = (gen_s0[24]) | 
+                        (prop_s0[24] & carry_int[24]);
+        
+        carry_int[26] = (gen_s0[25]) | 
+                        (prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[25] & prop_s0[24] & carry_int[24]);
+        
+        carry_int[27] = (gen_s0[26]) | 
+                        (prop_s0[26] & gen_s0[25]) |
+                        (prop_s0[26] & prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[26] & prop_s0[25] & prop_s0[24] & carry_int[24]);
+        
+        carry_int[28] = (gen_s0[27]) | 
+                        (prop_s0[27] & gen_s0[26]) |
+                        (prop_s0[27] & prop_s0[26] & gen_s0[25]) |
+                        (prop_s0[27] & prop_s0[26] & prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[27] & prop_s0[26] & prop_s0[25] & prop_s0[24] & carry_int[24]);
+        
+        carry_int[29] = (gen_s0[28]) |
+                        (prop_s0[28] & gen_s0[27]) | 
+                        (prop_s0[28] & prop_s0[27] & gen_s0[26]) |
+                        (prop_s0[28] & prop_s0[27] & prop_s0[26] & gen_s0[25]) |
+                        (prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & prop_s0[24] & carry_int[24]);
+    
+        carry_int[30] = (gen_s0[29]) |
+                        (prop_s0[29] & gen_s0[28]) |
+                        (prop_s0[29] & prop_s0[28] & gen_s0[27]) |
+                        (prop_s0[29] & prop_s0[28] & prop_s0[27] & gen_s0[26]) |
+                        (prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & gen_s0[25]) |
+                        (prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & prop_s0[24] & carry_int[24]);
+        
+        carry_int[31] = (gen_s0[30]) | 
+                        (prop_s0[30] & gen_s0[29]) |
+                        (prop_s0[30] & prop_s0[29] & gen_s0[28]) |
+                        (prop_s0[30] & prop_s0[29] & prop_s0[28] & gen_s0[27]) |
+                        (prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & gen_s0[26]) |
+                        (prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & gen_s0[25]) |
+                        (prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & prop_s0[24] & carry_int[24]);
+        
+        carry_int[32] = (gen_s0[31]) | 
+                        (prop_s0[31] & gen_s0[30]) |
+                        (prop_s0[31] & prop_s0[30] & gen_s0[29]) |
+                        (prop_s0[31] & prop_s0[30] & prop_s0[29] & gen_s0[28]) |
+                        (prop_s0[31] & prop_s0[30] & prop_s0[29] & prop_s0[28] & gen_s0[27]) |
+                        (prop_s0[31] & prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & gen_s0[26]) |
+                        (prop_s0[31] & prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & gen_s0[25]) |
+                        (prop_s0[31] & prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & gen_s0[24]) |
+                        (prop_s0[31] & prop_s0[30] & prop_s0[29] & prop_s0[28] & prop_s0[27] & prop_s0[26] & prop_s0[25] & prop_s0[24] & carry_int[24]);
+                            
         
         // Now, compute sums
         // we use the intermediate carrys for each group of 8.
-        sum_o[ 7: 0] = opX[ 7: 0] ^ opY_int[ 7: 0] ^ {8{cin_i}};
-        sum_o[15: 8] = opX[15: 8] ^ opY_int[15: 8] ^ {8{carry_int[0]}};
-        sum_o[23:16] = opX[23:16] ^ opY_int[23:16] ^ {8{carry_int[1]}};
-        sum_o[31:24] = opX[31:24] ^ opY_int[31:24] ^ {8{carry_int[2]}};
+        for (int i=0; i<32; i=i+1) begin
+		      sum_o[i] = opX_i[i] ^ opY_int[i] ^ carry_int[i];
+		  end
+		  cout_o = carry_int[32];
     end
 endmodule
--- in-order-pipeline-ideal-memory/ver/tb_ajw_addsub_unit.sv
+++ in-order-pipeline-ideal-memory/ver/tb_ajw_addsub_unit.sv
@@ -1,3 +1,32 @@
 module tb_ajw_addsub_unit;
-    // TODO: Create TB to verify our CLA.......
+    
+    int unsigned seed;
+    
+    localparam L_PERIOD = 20000;
+    
+    logic [31:0] tb_opX;
+    logic [31:0] tb_opY;
+    logic        tb_cin;
+    logic [31:0] tb_sum;
+    logic        tb_cout;
+    ajw_addsub_unit
+        u_dut_addsub_unit (
+            .opX_i(tb_opX),
+            .opY_i(tb_opY),
+            .cin_i(tb_cin),
+            .sum_o(tb_sum),
+            .cout_o(tb_cout)
+    );
+	int i;
+    initial begin : tests
+        for (i=0; i<24; i=i+1) begin
+			seed   = $urandom_range(1000, 1);
+            tb_opX = $urandom(seed+1'b1);
+            tb_opY = $urandom(seed-1'b1);
+            tb_cin = $urandom(seed+2'b10);
+            #(L_PERIOD);
+        end
+    end 
 endmodule
+
+
